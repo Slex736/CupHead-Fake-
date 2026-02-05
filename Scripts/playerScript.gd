7f0836b8925a01a95291217cd73dae4a4df598e8
Slex736 <amtjkersten@gmail.com>
2026-02-05 23:17:39 +0100

    added more graphics

--- Scripts/playerScript.gd
+++ Scripts/playerScript.gd
@@ -1,33 +1,45 @@
 extends CharacterBody2D
 
 
-@export var SPEED = FLOORSPEED
-@export var FLOORSPEED = 150.00
-@export var AIRSPEED = FLOORSPEED / 1.5
-@export var JUMP_VELOCITY = -300.0
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
+
+
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var jump_sound: AudioStreamPlayer2D = $JumpSound
+
+var platform_velocity := Vector2.ZERO
 
 
 func _physics_process(delta: float) -> void:
-	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		SPEED = AIRSPEED
-	else:
-		SPEED = FLOORSPEED
-		
-	
+		animated_sprite.play("Jump")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump_sound.play()
+		
+		
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# get input derection left = -1 and right = 1
 	var direction := Input.get_axis("Left", "Right")
-	if direction:
+	if direction == 1:
 		velocity.x = direction * SPEED
+		
+		animated_sprite.flip_h = false
+		
+	elif direction == -1:
+		velocity.x = direction * SPEED
+		
+		animated_sprite.flip_h = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animated_sprite.play("Idle")
+	
+	if not direction == 0:
+		animated_sprite.play("Walking") 
 
 	move_and_slide()
