0e40eac5933900b02b33024b5abf23e84a8d96c5
Slex736 <amtjkersten@gmail.com>
2026-04-01 22:36:09 +0200

    cactus boss attack 1

---
+++ Scripts/cactus_boss.gd
@@ -0,0 +1,42 @@
+extends Area2D
+
+
+@onready var player: CharacterBody2D = $"../../Player"
+
+var SpikeThrowScene = preload("res://Scenes/Enemies/BossAttackProjectiles/spike_throw.tscn")
+var SpikeFallScene = preload("res://Scenes/Enemies/BossAttackProjectiles/spike_ball_fall.tscn")
+
+
+enum CactusBossStates {
+	Idle,
+	SpikeThrow,
+	SpikeFall,
+}
+
+var CactusBossState = CactusBossStates.Idle
+
+
+func _process(_delta: float) -> void:
+	if randi() % 500 == 0:
+		CactusBossState = CactusBossStates.SpikeThrow
+	
+	
+	if CactusBossState == CactusBossStates.SpikeThrow:
+		SpikeThrow()
+	elif CactusBossState == CactusBossStates.SpikeFall:
+		SpikeFall()
+
+
+# load Spike Throw and give info so it can use bezier func to make an arch
+func SpikeThrow():
+	var SpikeThrowInstance = SpikeThrowScene.instantiate()
+	SpikeThrowInstance.Init(player.global_position, global_position)
+	add_child(SpikeThrowInstance)
+	
+
+	
+	CactusBossState = CactusBossStates.Idle
+
+
+func SpikeFall():
+	pass
---
+++ Scripts/spike.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+var speed = 200
+
+var dir: Vector2 
+var angle
+
+var StartPos: Vector2
+
+func _ready() -> void:
+	rotation = angle
+	StartPos = global_position
+
+func _process(delta: float) -> void:
+	global_position += dir * speed * delta
+	
+	var Distance = (StartPos - global_position).length()
+	if Distance > 1000:
+		queue_free()
+
+
+func PlayerEntered(body: Node2D) -> void:
+	if body is CharacterBody2D:
+		GameState.OpenInGameSettings()
